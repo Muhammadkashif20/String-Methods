80ef2a3f0450322bc0ea311e0b04887c7ebb3130
muhammadkashif20 <kashif28@gmail.com>
2024-05-15 10:15:50 +0500

    7 Questions Complete

--- app.js
+++ app.js
@@ -9,8 +9,52 @@ alert(`Assalam-ul-Alikum ${fullName}`)*/
 
 // Q2
 
-let userfavMobile=prompt('Enter Your Favourite Mobile')
+/*let userfavMobile=prompt('Enter Your Favourite Mobile')
 let userLength=userfavMobile.length
-alert(`My Favouite phone is ${userfavMobile} Length of string ${userLength}`)
+alert(`My Favouite phone is ${userfavMobile} Length of string ${userLength}`)*/
+
+// Q3
+
+/* let str='Pakistani'
+document.write(`Strings: ${str} <br>'`)
+let find=str.indexOf('n')
+document.write(`Index of 'n': ${find}`) */
+
+// Q4
+
+/* let str='Hello World';
+document.write(`String: ${str} <br>` )
+let lastFind=str.lastIndexOf('l')
+document.write(`Last index of 'l': ${lastFind}`) */
+
+// Q5
+
+/* let str='Pakistani';
+document.write(`String: ${str} <br>` )
+let findStr=str.charAt(3) 
+document.write(`Character at index 3: ${findStr}` )
+ */
+
+// Q6
+
+/* let firstName=prompt('Enter your First Name ');
+let lastName=prompt('Enter your Last Name');
+let fullName=firstName.concat(lastName)
+alert(`Assalam-ul-Alikum ${fullName}`)
+ */
+
+// Q7
+
+/* let City='Hyderabad';
+document.write(`City: ${City} <br>`)
+let Replace=City.replace('Hyderabad','Islamabad')
+document.write(`After Replacement: ${Replace}`) */
+
+
+
+
+
+
+
 
 
