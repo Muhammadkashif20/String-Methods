03aaea4845ae7a7a06f9234350430f54c0afb693
muhammadkashif20 <kashif28@gmail.com>
2024-05-16 12:19:22 +0500

    3 questions panding

--- app.js
+++ app.js
@@ -2,93 +2,93 @@
 
 // Q1
 
-/*let firstName=prompt('Enter your First Name');
-let lastName=prompt('Enter your Last Name');
-let fullName=firstName+' '+lastName;
-alert(`Assalam-ul-Alikum ${fullName}`)*/
+let fsName=prompt('Enter your First Name');
+let lsName=prompt('Enter your Last Name');
+let flName=fsName+' '+lsName;
+alert(`Assalam-ul-Alikum ${flName}`)
 
 // Q2
 
-/*let userfavMobile=prompt('Enter Your Favourite Mobile')
+let userfavMobile=prompt('Enter Your Favourite Mobile')
 let userLength=userfavMobile.length
-alert(`My Favouite phone is ${userfavMobile} Length of string ${userLength}`)*/
+alert(`My Favouite phone is ${userfavMobile} Length of string ${userLength}`)
 
 // Q3
 
-/* let str='Pakistani'
-document.write(`Strings: ${str} <br>'`)
-let find=str.indexOf('n')
-document.write(`Index of 'n': ${find}`) */
+let strs='Pakistani'
+document.write(`Strings: ${strs} <br>'`)
+let find=strs.indexOf('n')
+document.write(`Index of 'n': ${find}`)
 
 // Q4
 
-/* let str='Hello World';
-document.write(`String: ${str} <br>` )
-let lastFind=str.lastIndexOf('l')
-document.write(`Last index of 'l': ${lastFind}`) */
+let Strings='Hello World';
+document.write(`String: ${Strings} <br>` )
+let lastFind=Strings.lastIndexOf('l')
+document.write(`Last index of 'l': ${lastFind}`)
 
 // Q5
 
-/* let str='Pakistani';
-document.write(`String: ${str} <br>` )
+let string='Pakistani';
+document.write(`String: ${string} <br>` )
 let findStr=str.charAt(3) 
 document.write(`Character at index 3: ${findStr}` )
- */
+
 
 // Q6
 
-/* let firstName=prompt('Enter your First Name ');
+let firstName=prompt('Enter your First Name ');
 let lastName=prompt('Enter your Last Name');
 let fullName=firstName.concat(lastName)
 alert(`Assalam-ul-Alikum ${fullName}`)
- */
+
 
 // Q7
 
-/* let City='Hyderabad';
+let City='Hyderabad';
 document.write(`City: ${City} <br>`)
 let Replace=City.replace('Hyderabad','Islamabad')
-document.write(`After Replacement: ${Replace}`) */
+document.write(`After Replacement: ${Replace}`)
 
 // Q8
 
-/* let message = `Ali and Sami are best friends .They play cricket and
+let message = `Ali and Sami are best friends .They play cricket and
 football together.`;
 document.write(`${message} <br>`);
-let Replace = message.replaceAll("and", "&");
-document.write(Replace); */
+let Replaces = message.replaceAll("and", "&");
+document.write(Replaces);
 
 // Q9
 
-/* str="472"
+str="472"
 document.write(`Value: ${str} <br>`)
 document.write('Type:',typeof str+'<br>')
 document.write(`Value: ${str} <br>`)
 var str=Number()
-document.write('Type:',typeof str+'<br>') */
+document.write('Type:',typeof str+'<br>')
 
 // Q10
 
-/* let userInput=prompt('enter your Dry Fruits')
-document.write(`User input: ${userInput} <br>`)
-let convert=userInput.toUpperCase()
-document.write(`Upper Case ${convert}`) */
+let userInput1=prompt('enter your Dry Fruits')
+document.write(`User input: ${userInput1} <br>`)
+let convert2=userInput1.toUpperCase()
+document.write(`Upper Case ${convert2}`)
 
 // Q11
 
-/* let userInput=prompt('enter your letter')
-document.write(`User input: ${userInput} <br>`)
-let convert=userInput.toUpperCase().slice(0,1)
+let userInputs=prompt('enter your letter')
+document.write(`User input: ${userInputs} <br>`)
+let change=userInputs.toUpperCase().slice(0,1)
 // console.log(convert);
-document.write(`Title Case: ${convert}`+userInput.slice(1)) */
+document.write(`Title Case: ${change}`+userInputs.slice(1))
 
 // Q12
 
-/* let num=[35.36]
+let num=[35.36]
 document.write(`Number: ${num} <br>`)
 let join=num.splice(2,1)
 document.write(join);
- */
+
 
 // Q13 no clear
 
@@ -105,7 +105,7 @@ document.write(join);
 
 // Q14
 
-/* let userBakery=prompt('enter your items please check list available items')
+let userBakery=prompt('enter your items please check list available items')
 let arr= ['cake', 'apple pie', 'cookie', 'chips', 'patties']
 for(let i=0;i<arr.length;i++){
     if(userBakery.toLowerCase()===arr[i]){
@@ -116,7 +116,7 @@ for(let i=0;i<arr.length;i++){
         alert(`We are Sorry. ${userBakery} is not avalable in our Bakery`)
  
     }
-} */
+}
 
 // Q15 no clear
 
@@ -124,28 +124,28 @@ for(let i=0;i<arr.length;i++){
 
 // Q16
 
-/* let university = ['University of Karachi'];
+let university = ['University of Karachi'];
 let convert=university.toString()
 let convert1=convert.split(',')
-document.write(convert1) */
+document.write(convert1)
 
 
 // Q17
 
-/* let userInput='pakistan'
+let userInput='pakistan'
 document.write(`User Input: ${userInput} <br>`)
-let convert=userInput.charAt(7)
-document.write(`Last Characters of input:${convert}`) */
+let converts=userInput.charAt(7)
+document.write(`Last Characters of input:${converts}`)
 
 // Q18 no clear
 
-/* let str='The quick brown fox jumps over the lazy dog';
+let str='The quick brown fox jumps over the lazy dog';
 let count=0
 for(let i=0;i<str.length;i++){
     if(str[i]==='The'){
         document.write(str)
     }
-} */
+}
 
 
 
