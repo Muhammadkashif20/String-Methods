73bbb22f22306a0a273566e5b9c07d2df4b6b8fc
muhammadkashif20 <kashif28@gmail.com>
2024-05-16 12:09:53 +0500

    3 question no clear

--- app.js
+++ app.js
@@ -76,17 +76,81 @@ document.write(`Upper Case ${convert}`) */
 
 // Q11
 
- /* let userInput=prompt('enter your letter')
- document.write(`User input: ${userInput} <br>`)
-let convert=userInput.replace(/\b\w/g, a=> a.toUpperCase())
-document.write(convert) */
+/* let userInput=prompt('enter your letter')
+document.write(`User input: ${userInput} <br>`)
+let convert=userInput.toUpperCase().slice(0,1)
+// console.log(convert);
+document.write(`Title Case: ${convert}`+userInput.slice(1)) */
 
 // Q12
 
-// let str=35.36
-// document.write(`Number: ${str} <br>`)
-// let convert=str.
-// document.write('Result: '+str.join(''))
+/* let num=[35.36]
+document.write(`Number: ${num} <br>`)
+let join=num.splice(2,1)
+document.write(join);
+ */
+
+// Q13 no clear
+
+// let userName=prompt('Enter your Name');
+// let specialChar=['@',',','.','!']
+// let store=userName;
+
+// if(userName===store){
+    
+// }
+// else if(userName===specialChar){
+// alert('please enter a valid username')
+// }
+
+// Q14
+
+/* let userBakery=prompt('enter your items please check list available items')
+let arr= ['cake', 'apple pie', 'cookie', 'chips', 'patties']
+for(let i=0;i<arr.length;i++){
+    if(userBakery.toLowerCase()===arr[i]){
+        alert(`${userBakery} is Available at index ${i} in our Bakery`)
+        break  
+    }
+    else{
+        alert(`We are Sorry. ${userBakery} is not avalable in our Bakery`)
+ 
+    }
+} */
+
+// Q15 no clear
+
+// let userPassword=prompt('Enter your Password');
+
+// Q16
+
+/* let university = ['University of Karachi'];
+let convert=university.toString()
+let convert1=convert.split(',')
+document.write(convert1) */
+
+
+// Q17
+
+/* let userInput='pakistan'
+document.write(`User Input: ${userInput} <br>`)
+let convert=userInput.charAt(7)
+document.write(`Last Characters of input:${convert}`) */
+
+// Q18 no clear
+
+/* let str='The quick brown fox jumps over the lazy dog';
+let count=0
+for(let i=0;i<str.length;i++){
+    if(str[i]==='The'){
+        document.write(str)
+    }
+} */
+
+
+
+
+
 
 
 
